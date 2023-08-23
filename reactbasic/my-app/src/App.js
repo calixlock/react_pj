48027aad67cc8418eee5a2c29896034f5f7aba9b
calixlock <calixlock@naver.com>
2023-08-23 17:35:39 +0900

    react useState 적용

--- reactbasic/my-app/src/App.js
+++ reactbasic/my-app/src/App.js
@@ -1,7 +1,9 @@
 import React from "react"; // 작성된 파일 불러와 App 함수의 return 값에 jsx 문법으로 사용
 
 import { Link, Route, Routes } from "react-router-dom"; // route 관련 라이브 러리
-import About from "./pages/About"; // page 불러오기
+// page 불러오기
+import About from "./pages/About";
+import Counter from "./pages/Counter";
 import Home from "./pages/Home";
 
 function App() {
@@ -13,12 +15,14 @@ function App() {
       <nav>
         {/* <a href="/">Home</a> 동일 */}
         {/* Link는 a태그로 변환 적용 */}
-        <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;<Link to="/about">About</Link>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/Counter">Counter</Link>
       </nav>
       <hr />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/Counter" element={<Counter />} />
       </Routes>
       <hr />
     </div>
