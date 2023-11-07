ed8972c570c9931e91b611658302034253769091
calixlock <calixlock@naver.com>
2023-11-07 18:01:18 +0900

    navbar css 적용

--- vite-react-route-ex/src/components/Header.jsx
+++ vite-react-route-ex/src/components/Header.jsx
@@ -4,11 +4,10 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="header-container">
-      Header
       <div className="header-wrap">
         <div className="header-left-wrap">
           {/* 클릭시 이동되는 Link tag 이용 */}
-          <Link to="/">
+          <Link style={{ display: "flex", alignItems: "center" }} to="/">
             <img
               style={{ width: "154px", height: "20px" }}
               src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
