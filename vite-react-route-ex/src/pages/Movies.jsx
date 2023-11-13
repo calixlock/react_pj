cb8e7b135e35aba4c61f1bf2a12e847cbb33d500
calixlock <calixlock@naver.com>
2023-11-13 23:54:50 +0900

    dummy update

--- vite-react-route-ex/src/pages/Movies.jsx
+++ vite-react-route-ex/src/pages/Movies.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-
+import { dummy } from "../movieDummy";
 export default function Movies() {
-  return <div>Movies</div>;
+  return (
+    <div>
+      <div>Movies</div>
+      <div className="movie-container">
+        {dummy.results.map((item) => {
+          return <div>{item.id}</div>;
+        })}
+      </div>
+    </div>
+  );
 }
