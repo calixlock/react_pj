9436ca55a0b47f5cd5e1b20057aad43ad37e7f5a
calixlock <calixlock@naver.com>
2024-11-07 03:58:02 +0900

    add potato component

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -1,6 +1,14 @@
+import Potato from "./potato";
 function App() {
   // return <div className="App" />;
-  return <h1>hello react</h1>;
+  return (
+    <div>
+      <h1>hello react</h1>
+      <div>
+        <Potato />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ movie_app_2024/src/potato.js
@@ -0,0 +1,5 @@
+function Potato(params) {
+  return <h3>this is potato</h3>;
+}
+
+export default Potato;
