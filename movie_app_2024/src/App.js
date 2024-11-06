1e99d484d5e68715438fbf3c9b3b4305e4949fce
calixlock <calixlock@naver.com>
2024-11-07 04:34:03 +0900

    props 활용

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -1,5 +1,6 @@
-function Potato(params) {
-  return <h3>this is potato.v2</h3>;
+function Food({ fav }) {
+  // console.log({ fav });
+  return <h3>{fav} is good.</h3>;
 }
 function App() {
   // return <div className="App" />;
@@ -7,7 +8,11 @@ function App() {
     <div>
       <h1>hello react</h1>
       <div>
-        <Potato />
+        {/* <Food /> */}
+        <Food fav="spam" p1="true" p2={true} p3={[1, 2, 3, 4, "hello"]} />
+        <Food fav="stake" />
+        <Food fav="apple" />
+        <Food fav="?" />
       </div>
     </div>
   );
