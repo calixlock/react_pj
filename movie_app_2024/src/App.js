3aebb53b05dabadf9bf1a49e272b4afa94df6375
calixlock <calixlock@naver.com>
2024-11-08 04:21:54 +0900

    axios를 통해 가져온 api 데이터를 state 데이터에 동기화 시킨후 loading에서 movie로 변경되도록 셋팅

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -8,15 +8,21 @@ class App extends React.Component {
   // async 링크를 받아오는 시간이 필요하므로 // 비동기 처리
   getMovies = async () => {
     // await를 통해 기다리고 진행하도록
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
     console.log(movies);
+    // this.setState({ movies: movies });
+    this.setState({ movies, isLoading: 0 });
   };
   componentDidMount() {
-    this.getMovies();
+    this.getMovies(); // getMovies() 메서드를 호출하여 영화 데이터를 가져오는 비동기 작업을 시작
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading..." : "movies"}</div>;
   }
 }
 export default App;
