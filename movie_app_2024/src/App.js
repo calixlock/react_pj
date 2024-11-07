abbb1672dd871a97affd8bbb09e1d6ae940748f7
calixlock <calixlock@naver.com>
2024-11-08 02:26:10 +0900

    component render 생명주기 test

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -2,6 +2,23 @@ import React from "react";
 
 //react의 component 상속
 class App extends React.Component {
+  //class 생성 시 constructor 호출
+  constructor(props) {
+    super(props);
+    console.log("constructor !");
+  }
+  // 최초 실행 시 render 된 후
+  componentDidMount() {
+    console.log("componentDidMount !");
+  }
+  // update 될때만 reder 된 후
+  componentDidUpdate() {
+    console.log("componentDidUpdate !");
+  }
+  // 컴퍼넌트가 화면에서 떠날때 실행됨
+  componentWillUnmount() {
+    console.log("componentWillUnmout !");
+  }
   state = {
     count: 0,
   };
@@ -14,6 +31,7 @@ class App extends React.Component {
     this.setState({ count: this.state.count - 1 });
   };
   render() {
+    console.log("render !");
     return (
       <div>
         <h1> number : {this.state.count}</h1>
