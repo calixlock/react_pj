f8ae8fc36ec3f1da73ac2aabafea7a4bcec6732a
calixlock <calixlock@naver.com>
2024-11-08 04:11:23 +0900

    axios 패키지 설치후 movie_data_api data 비동기 처리

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -1,17 +1,22 @@
+import axios from "axios";
 import React from "react";
 class App extends React.Component {
   state = {
     isLoading: 1,
-    movies: ["m1", "m2"],
+    movies: [],
+  };
+  // async 링크를 받아오는 시간이 필요하므로 // 비동기 처리
+  getMovies = async () => {
+    // await를 통해 기다리고 진행하도록
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
   };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: 0 });
-    }, 2000);
+    this.getMovies();
   }
   render() {
-    const { isLoading, movies } = this.state;
-    return <div>{isLoading ? "Loading..." : movies}</div>;
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "ready"}</div>;
   }
 }
 export default App;
