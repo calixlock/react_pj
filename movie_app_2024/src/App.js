ba86449b1c0abacac676b9167d5d497c216196eb
calixlock <calixlock@naver.com>
2024-11-07 18:57:17 +0900

    add props type npm install 후 기능 적용

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -1,9 +1,12 @@
-function Food({ index, name, img }) {
+import PropTypes from "prop-types";
+
+function Food({ index, name, img, rating }) {
   return (
     <div>
       <h2>
         {index + 1}. {name}
       </h2>
+      <h4>{rating} / 5.0</h4>
       <img src={img} alt={name} />
     </div>
   );
@@ -22,28 +25,37 @@ const foodList = [
     name: "stake",
     image:
       "https://cdn.pixabay.com/photo/2021/12/20/14/52/chicken-6883246_1280.jpg",
-    rating: 4.9,
   },
   {
     id: 3,
     name: "apple",
     image:
       "https://cdn.pixabay.com/photo/2019/02/04/06/45/apple-3974055_1280.jpg",
-    rating: 4.2,
+    rating: 3.2,
   },
 ];
-const renderFood = (item, idx) => (
-  <Food key={idx} name={item.name} img={item.image} index={idx} />
-);
 
 function App() {
-  console.log(foodList.map(renderFood));
   return (
     <div>
       <h1>Food List</h1>
-      {foodList.map(renderFood)}
+      {foodList.map((item, idx) => (
+        <Food
+          key={idx}
+          name={item.name}
+          img={item.image}
+          index={idx}
+          rating={item.rating}
+        />
+      ))}
     </div>
   );
 }
+// props의 type 체크 / 불충족 시 console에 warning 발생
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 export default App;
