e66abe0a5b13b6ac21a96cd37f95c738f00347b6
calixlock <calixlock@naver.com>
2024-11-07 18:36:07 +0900

    add props idx key alt check

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -1,8 +1,8 @@
-function Food({ idx, name, img }) {
+function Food({ index, name, img }) {
   return (
     <div>
       <h2>
-        {idx + 1}. {name}
+        {index + 1}. {name}
       </h2>
       <img src={img} alt={name} />
     </div>
@@ -11,23 +11,29 @@ function Food({ idx, name, img }) {
 
 const foodList = [
   {
+    id: 1,
     name: "spam",
     image:
       "https://cdn.pixabay.com/photo/2015/08/27/04/31/sapm-909485_1280.jpg",
+    rating: 5,
   },
   {
+    id: 2,
     name: "stake",
     image:
       "https://cdn.pixabay.com/photo/2021/12/20/14/52/chicken-6883246_1280.jpg",
+    rating: 4.9,
   },
   {
+    id: 3,
     name: "apple",
     image:
       "https://cdn.pixabay.com/photo/2019/02/04/06/45/apple-3974055_1280.jpg",
+    rating: 4.2,
   },
 ];
 const renderFood = (item, idx) => (
-  <Food key={idx} name={item.name} idx={idx} img={item.image} />
+  <Food key={idx} name={item.name} img={item.image} index={idx} />
 );
 
 function App() {
