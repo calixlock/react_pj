bce00a84eff77ab46e712cd8f206befd730f9c5e
calixlock <calixlock@naver.com>
2024-11-07 16:04:44 +0900

    props이용해서 json형식 활용

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -1,19 +1,39 @@
-function Food({ fav }) {
-  // console.log({ fav });
-  return <h3>{fav} is good.</h3>;
+function Food({ idx, name, img }) {
+  return (
+    <div>
+      <h2>
+        {idx + 1}. {name}
+      </h2>
+      <img src={img} alt={name} />
+    </div>
+  );
 }
+
+const foodList = [
+  {
+    name: "spam",
+    image:
+      "https://cdn.pixabay.com/photo/2015/08/27/04/31/sapm-909485_1280.jpg",
+  },
+  {
+    name: "stake",
+    image:
+      "https://cdn.pixabay.com/photo/2021/12/20/14/52/chicken-6883246_1280.jpg",
+  },
+  {
+    name: "apple",
+    image:
+      "https://cdn.pixabay.com/photo/2019/02/04/06/45/apple-3974055_1280.jpg",
+  },
+];
 function App() {
   // return <div className="App" />;
   return (
     <div>
-      <h1>hello react</h1>
-      <div>
-        {/* <Food /> */}
-        <Food fav="spam" p1="true" p2={true} p3={[1, 2, 3, 4, "hello"]} />
-        <Food fav="stake" />
-        <Food fav="apple" />
-        <Food fav="?" />
-      </div>
+      <h1>Food List</h1>
+      {foodList.map((item, idx) => (
+        <Food key={idx} name={item.name} idx={idx} img={item.image} />
+      ))}
     </div>
   );
 }
