9af9a07d4166623b811c2f88a7e57cff714237fe
calixlock <calixlock@naver.com>
2024-11-08 12:11:19 +0900

    poster update

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -42,7 +42,7 @@ class App extends React.Component {
                   year={movie.year}
                   title={movie.title}
                   summary={movie.summary}
-                  poster={movie.poster}
+                  poster={movie.medium_cover_image}
                 />
               );
             })}
--- movie_app_2024/src/Movies.js
+++ movie_app_2024/src/Movies.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 const Movie = ({ id, year, title, summary, poster }) => {
   return (
     <div class="movie_data">
+      <img src={poster} alt={title} title={title} />
       <h3 class="movie_title">title : {title}</h3>
       <h5 class="movie_year">year : {year}</h5>
       <p class="movie_summary">summary : {summary}</p>
-      {/* <h4>poster : {poster}</h4> */}
       <hr />
     </div>
   );
@@ -16,8 +16,8 @@ Movie.propTypes = {
   id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  // summary: PropTypes.string.isRequired,
-  // poster: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
 };
 
 export default Movie;
