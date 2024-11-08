bf166100f2fbe4df58da91f69898c915b1acbcf2
calixlock <calixlock@naver.com>
2024-11-08 21:08:05 +0900

    장르 ul > li 개별출력

--- movie_app_2024/src/Movie.js
+++ movie_app_2024/src/Movie.js
@@ -2,12 +2,18 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 const Movie = ({ year, title, summary, poster, genres }) => {
   return (
-    <div class="movie_data">
+    <div className="movie_data">
       <img src={poster} alt={title} title={title} />
-      <h3 class="movie_title">title : {title}</h3>
-      <h5 class="movie_genre">genre : {genres}</h5>
-      <h5 class="movie_year">year : {year}</h5>
-      <p class="movie_summary">summary : {summary}</p>
+      <h3 className="movie_title">title : {title}</h3>
+      {/* <h5 className="movie_genre">genre : {genres}</h5> */}
+      <ul className="movie_genres">
+        {genres.map((e) => {
+          return <li className="movie_genre">{e}</li>;
+        })}
+      </ul>
+
+      <h5 className="movie_year">year : {year}</h5>
+      <p className="movie_summary">summary : {summary}</p>
       <hr />
     </div>
   );
