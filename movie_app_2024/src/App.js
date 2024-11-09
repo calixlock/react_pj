10755e3dcfee1affe20652c73cb1dbf9a095a13e
calixlock <calixlock@naver.com>
2024-11-10 07:36:44 +0900

    router 적용

--- movie_app_2024/src/App.js
+++ movie_app_2024/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./routes/About";
 import Home from "./routes/Home";
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 }
 export default App;
